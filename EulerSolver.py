d36ffa95c8e6a20d8ebaaca54251ba6ab41a4aea
wjwitek <weronikaj.witek@gmail.com>
2021-12-30 23:21:56 +0100

    Add comments

--- EulerSolver.py
+++ EulerSolver.py
@@ -1,6 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
+import platform
+
+
+def get_plots_path():
+    if "Windows" in platform.system():
+        return "plots\\"
+    else:
+        return "plots/"
+
+
+"""
+Class performing Euler algorithm and keeping current estimated values.
+"""
 
 
 class EulerSolver:
@@ -9,10 +22,15 @@ class EulerSolver:
         self.h = step_size
         self.y0 = initial_condition
         self.ode = ode  # solved differential equation, as lambda
-        self.y = None
-        self.x = None
+        self.y = []
+        self.x = []
 
-    def explicit_euler_method(self, a, b):
+    def explicit_euler_method(self, a=0, b=1):
+        """
+        Calculate y(x) for each x.
+        :param a: start of interval (inclusive)
+        :param b: end of interval (inclusive)
+        """
         # a, b - rage on which equation is solved
         self.x = np.arange(a, b + self.h, self.h)  # Numerical grid
         self.y = np.zeros(len(self.x))
@@ -22,10 +40,15 @@ class EulerSolver:
             self.y[i + 1] = self.y[i] + self.h * self.ode(self.x[i], self.y[i])
 
     def plot(self, file_path):
+        """
+        Plot current x and y, compared to explicit solution.
+        :param file_path: where to save plot
+        """
         plt.style.use('seaborn-poster')
         plt.figure(figsize=(12, 8))
-        plt.plot(self.y, self.x, 'bo--', label='Przybliżone')
-        plt.plot(self.x, -np.exp(-self.x), 'g', label='Dokładne')
+        plt.plot(self.x, self.y, 'bo--', label='Przybliżone')
+        if self.exact_solution is not None:
+            plt.plot(self.x, self.exact_solution(self.x), 'g', label='Dokładne')
         plt.title('Dokładne i przybliżone rozwiązania')
         plt.xlabel('x')
         plt.ylabel('f(x)')
@@ -34,12 +57,21 @@ class EulerSolver:
         plt.savefig(file_path)
         plt.close()
 
-    def step(self, a, b, file_path):
+    def step(self, a=0, b=1):
+        """
+        Make step smaller, repeat calculating y(x) and redraw plot.
+        :param a: start of interval (inclusive)
+        :param b: end of interval (inclusive)
+        """
         self.h /= 10
         self.explicit_euler_method(a, b)
-        self.plot(file_path)
+        self.plot(get_plots_path() + "current_plot.png")
 
     def save_state(self, file_path):
+        """
+        Save current x and y(x) to csv file.
+        :param file_path: where to save file
+        """
         # open the file in the write mode
         with open(file_path, 'w') as f:
             # create the csv writer
@@ -47,10 +79,14 @@ class EulerSolver:
 
             # add header
             header = ["x", "y"]
+            if self.exact_solution is not None:
+                header.append("y_true")
             writer.writerow(header)
 
             # add data
             for i in range(len(self.y)):
-                row = [self.x[i], self.y[i]]
+                row = [round(self.x[i], 2), round(self.y[i], 2)]
+                if self.exact_solution is not None:
+                    row.append(round(self.exact_solution(self.x[i]), 2))
                 # write a row to the csv file
                 writer.writerow(row)
